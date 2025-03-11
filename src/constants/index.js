39f0c138e1b48d47093012390510cfe28b8e84f7
alexmiranda18 <alexsousamiranda@hotmail.com>
2025-03-11 07:47:47 -0300

    docs: "add professional experience and new React project details"

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,12 +2,20 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpeg";
 
 export const HERO_CONTENT = `Sou um desenvolvedor full stack apaixonado, com talento para criar aplicações web robustas e escaláveis. aprimorei minhas habilidades em tecnologias front-end como React e Next.js, bem como tecnologias back-end como Node.js, MySQL, PostgreSQL. Meu objetivo é aproveitar minha experiência para criar soluções inovadoras que impulsionem o crescimento dos negócios e proporcionem experiências excepcionais ao usuário.`;
 
 export const ABOUT_TEXT = `Sou um desenvolvedor full stack dedicado e versátil, com uma paixão por criar aplicações web eficientes e intuitivas. Recentemente, fiz uma transição de carreira da área de eletrônica para o desenvolvimento de software, e estou sempre em busca de aprimorar meus conhecimentos. Durante um ano e meio, trabalhei em uma startup chamada Wooeen, onde fui responsável pelo tracking de e-commerces, o que me proporcionou uma valiosa experiência prática. Nesse período, estudei e apliquei tecnologias como React, Node.js e MySQL, consolidando meu conhecimento nessas áreas. Tenho 32 anos, sou casado e pai de um filho, e acredito que minha curiosidade sobre como as coisas funcionam foi o que inicialmente me atraiu para a programação. Desde então, essa curiosidade evoluiu para uma carreira na qual busco constantemente aprender e me adaptar a novos desafios. Gosto de trabalhar em ambientes colaborativos e de resolver problemas complexos para entregar soluções de alta qualidade. Fora do mundo da programação, adoro cinema e passar tempo com minha família.`;
 
 export const EXPERIENCES = [
+  {
+    year: "2024 - 2025",
+    role: "Suporte Técnico",
+    company: "Dev Club",
+    description: `Experiência oferecendo auxilio aos alunos, ajudando a resolver problemas de código e orientando em suas jornadas de aprendizado.`,
+    technologies: ["Node.js", "MongoDB", "React.js", "Html", "Css"],
+  },
   {
     year: "2022 - 2024",
     role: "Desenvolvedor",
@@ -40,6 +48,14 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
+    title: "Gerenciador de estoque StockPro",
+    image: project5,
+    description:
+      "Desenvolvi uma aplicação completa utilizando TypeScript, React, Node.js, Tailwind CSS, Supabase, Vercel e Render. O projeto inclui funcionalidades como upload de imagens com Multer, autenticação com JWT e criptografia de senhas com Bcrypt. Esta aplicação demonstra minha capacidade de integrar diversas tecnologias para criar uma solução robusta e funcional.",
+    technologies: ["TypeScript", "React", "Node.js", "Tailwind CSS"],
+    url: "https://inventory-portal-six.vercel.app/",
+  },
+  {
     title: "Website Portifólio",
     image: project1,
     description:
@@ -60,7 +76,15 @@ export const PROJECTS = [
     image: project3,
     description:
       "Projeto simples que simula o cadastro de pedidos,  tem o objetivo de integrar com o back-end na funcão de cadastrar, visualizar e deletar pedidos.",
-    technologies: ["HTML", "CSS", "React", "JavaScript", "Node.js", "Express.js", "MongoDB"],
+    technologies: [
+      "HTML",
+      "CSS",
+      "React",
+      "JavaScript",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+    ],
     url: "https://hamburgueria-react-alex.netlify.app",
   },
   {
